b61c7ceaa0487202cf41b1ba153e28dd77f48173
Gabrielius <Gabrielius.Vaiskunas@mif.stud.vu.lt>
2020-12-13 23:51:34 +0200

    Added minimax algorithm, removed FindEmptyCell functions

--- src/Task3.hs
+++ src/Task3.hs
@@ -83,27 +83,13 @@ hasWonAny :: Grid -> Bool
 hasWonAny grid = (hasWon grid 'X') || (hasWon grid 'O')
 
 
-findEmptyCellGrid :: Grid -> (Int, Int)
-findEmptyCellGrid grid = case isRowFull (t0 grid) of
-    False -> (0, findEmptyCellRow (t0 grid))
-    True -> case isRowFull (t1 grid) of
-        False -> (1, findEmptyCellRow (t1 grid))
-        True -> case isRowFull (t2 grid) of
-            False -> (2, findEmptyCellRow (t2 grid))
-            True -> (-1, -1)
-
-findEmptyCellRow :: Row -> Int
-findEmptyCellRow row = case t0 row == '_' of
-    True -> 0
-    False -> case t1 row == '_' of
-        True -> 1
-        False -> case t2 row == '_' of
-            True -> 2
-            False -> -1
-
-
 isXYValid :: Int -> Int -> Bool
 isXYValid x y = (x >= 0) && (x < 3) && (y >= 0) && (y < 3)
+
+
+invertSymbol :: Char -> Char
+invertSymbol 'X' = 'O'
+invertSymbol 'O' = 'X'
 
 
 showGrid :: Grid -> IO ()
@@ -196,5 +182,110 @@ parse msg =
 -- OUT: Bencoded string with new move appended
 encode :: String -> Int -> Int -> Char -> String
 encode msg x y v = "d4:lastld4:datali" ++ show x ++ "ei" ++ show y ++ "e1:" ++ [v] ++ "eee4:prev" ++ msg ++ "e"
+
+
+--                - - - M I N I M A X   A L G O R I T H M   F U N C T I O N S - - -
+
+
+-- Checks if anyone has won the game and returns the appropriate score
+-- IN: Game grid, current player's symbol
+-- OUT:  10 if current player has won
+--      -10 if current player's opponent has won
+--        0 if noone has won    
+evaluateGridScore :: Grid -> Char -> Int
+evaluateGridScore grid v 
+    | hasWon grid v = 10
+    | hasWon grid (invertSymbol v) = -10
+    | otherwise = 0
+
+-- Advances the passed X Y coordinates by 1, simple helper function
+-- IN: X, Y to advance
+-- OUT: X, Y advanced by one, X kept below 3
+advanceXY :: Int -> Int -> (Int, Int)
+advanceXY x y
+    | (x + 1) > 2 = (0, y + 1)
+    | otherwise = (x + 1, y)
+
+
+-- Checks if any additional moves can be made, and advances the minimax algorithm if additional moves are possible
+-- IN: Game grid, move depth, real player, currently checked player
+-- OUT: Best score
+minimax :: Grid -> Int -> Char -> Char -> Int
+minimax grid depth rv v 
+    | score ==  10 = score - depth
+    | score == -10 = score + depth
+    | isGridFull grid = 0
+    | otherwise = callMinimaxOnGrid grid (depth + 1) rv (invertSymbol v)
+    where
+        score = evaluateGridScore grid rv
+
+
+-- Attempts to call minimax on the given cell. If cell is not already filled in, will fill it and call minimax.
+-- IN: Game grid, move depth, real player, currently checked player, cell x, cell y
+-- OUT: Best score
+tryCallMinimax :: Grid -> Int -> Char -> Char -> Int -> Int -> Int
+tryCallMinimax grid depth rv v x y
+    | getGridValue grid x y == '_' =
+        minimax (setGridValue grid v x y) depth rv v
+    | otherwise = 
+        case rv == v of
+            True -> -1000 -- Trying to maximize
+            False -> 1000 -- Trying to minimize
+
+
+-- Attempts to call minimax on the entire grid and gets the maximum/minimum score of all minimax calls
+-- IN: Game grid, move depth, real player, currently checked player 
+-- OUT: Best score
+callMinimaxOnGrid :: Grid -> Int -> Char -> Char -> Int
+callMinimaxOnGrid grid depth rv v
+    | rv == v = maximum values -- Trying to maximize
+    | otherwise = minimum values -- Trying to minimize
+    where 
+        values = [(tryCallMinimax grid depth rv v 0 0), (tryCallMinimax grid depth rv v 1 0), (tryCallMinimax grid depth rv v 2 0),
+                  (tryCallMinimax grid depth rv v 0 1), (tryCallMinimax grid depth rv v 1 1), (tryCallMinimax grid depth rv v 2 1),
+                  (tryCallMinimax grid depth rv v 0 2), (tryCallMinimax grid depth rv v 1 2), (tryCallMinimax grid depth rv v 2 2)]
+        
+
+-- Finds the move with the highest score for the given grid and player
+-- IN: Game grid, player, cell x, cell y
+-- OUT: Best move x, best move y, best score
+findBestMove' :: Grid -> Char -> Int -> Int -> (Int, Int, Int)
+findBestMove' grid v x y 
+    | score > score' = (x, y, score)
+    | otherwise = (x'', y'', score')
+    where
+        score = tryCallMinimax grid 0 v v x y
+
+        (x', y') = advanceXY x y
+        (x'', y'', score') = case y' > 2 of
+            False -> findBestMove' grid v x' y'
+            True -> (0, 0, -1000) -- Reached the end of the grid
+
+
+-- Calls the findBestMove function and appends the appropriate exit code
+-- IN: Game grid, player
+-- OUT: Best move x, best move y, exit code
+findBestMove :: Grid -> Char -> (Int, Int, Int)
+findBestMove grid v = (x, y, code)
+    where
+        (x, y, _) = findBestMove' grid v 0 0
+        grid' = setGridValue grid v x y
+        code = case hasWonAny grid' of
+            True -> case hasWon grid' v of
+                True -> 10
+                False -> -1000 -- We should _not_ be here
+            False -> case isGridFull grid' of
+                True -> 12
+                False -> 0
+
+
+-- Checks if the game can continue to be played before calling findBestMove
+-- IN: Game grid, player
+-- OUT: Best move x, best move y, exit code
+findBestMovePregameCheck :: Grid -> Char -> (Int, Int, Int)
+findBestMovePregameCheck grid v 
+    | isGridFull grid = (0, 0, 20)
+    | hasWonAny grid = (0, 0, 20)
+    | otherwise = findBestMove grid v
 
 
