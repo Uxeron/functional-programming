82ea1420feff781f6f1107defb1c00d51cfcc22d
Gabrielius <Gabrielius.Vaiskunas@mif.stud.vu.lt>
2020-12-13 18:24:36 +0200

    Removed XY checking for set and get Grid functions, created separate validation function.

--- src/Task3.hs
+++ src/Task3.hs
@@ -1,5 +1,7 @@
 module Task3 where
 
+import Data.Char
+
 type Row = (Char, Char, Char)
 type Grid = (Row, Row, Row)
 
@@ -27,39 +29,26 @@ t2 (_, _, c) = c
 
 --                - - - G R I D   &   R O W   F U N C T I O N S - - -
 
-setGridValue :: Grid -> Int -> Int -> Char -> Either String Grid
-setGridValue grid 0 y v = 
-    case setRowValue (t0 grid) y v of
-        Right row -> Right (row, t1 grid, t2 grid)
-        Left err -> Left err
-setGridValue grid 1 y v = 
-    case setRowValue (t1 grid) y v of
-        Right row -> Right (t0 grid, row, t2 grid)
-        Left err -> Left err
-setGridValue grid 2 y v = 
-    case setRowValue (t2 grid) y v of
-        Right row -> Right (t0 grid, t1 grid, row)
-        Left err -> Left err
-setGridValue _ _ _ _ = Left "Invalid x index"
+setGridValue :: Grid -> Int -> Int -> Char -> Grid
+setGridValue grid 0 y v = (setRowValue (t0 grid) y v, t1 grid, t2 grid)
+setGridValue grid 1 y v = (t0 grid, setRowValue (t1 grid) y v, t2 grid)
+setGridValue grid 2 y v = (t0 grid, t1 grid, setRowValue (t2 grid) y v)
 
-setRowValue :: Row -> Int -> Char -> Either String Row
-setRowValue row 0 v = Right (v, t1 row, t2 row)
-setRowValue row 1 v = Right (t0 row, v, t2 row)
-setRowValue row 2 v = Right (t0 row, t1 row, v)
-setRowValue _ _ _ = Left "Invalid y index"
+setRowValue :: Row -> Int -> Char -> Row
+setRowValue row 0 v = (v, t1 row, t2 row)
+setRowValue row 1 v = (t0 row, v, t2 row)
+setRowValue row 2 v = (t0 row, t1 row, v)
 
 
-getGridValue :: Grid -> Int -> Int -> Either String Char
+getGridValue :: Grid -> Int -> Int -> Char
 getGridValue grid 0 y = getRowValue (t0 grid) y
 getGridValue grid 1 y = getRowValue (t1 grid) y
 getGridValue grid 2 y = getRowValue (t2 grid) y
-getGridValue _ _ _ = Left "Invalid x index"
 
-getRowValue :: Row -> Int -> Either String Char
-getRowValue row 0 = Right (t0 row)
-getRowValue row 1 = Right (t1 row)
-getRowValue row 2 = Right (t2 row)
-getRowValue _ _ = Left "Invalid y index"
+getRowValue :: Row -> Int -> Char
+getRowValue row 0 = t0 row
+getRowValue row 1 = t1 row
+getRowValue row 2 = t2 row
 
 
 isGridFull :: Grid -> Bool
@@ -67,3 +56,7 @@ isGridFull grid = isRowFull (t0 grid) && isRowFull (t1 grid) && isRowFull (t2 gr
 
 isRowFull :: Row -> Bool
 isRowFull row = (t0 row /= '_') && (t1 row /= '_') && (t2 row /= '_')
+
+
+isXYValid :: Int -> Int -> Bool
+isXYValid x y = (x >= 0) && (x < 3) && (y >= 0) && (y < 3)
